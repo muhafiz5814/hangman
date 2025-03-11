631ff755a4f3bc39f86563452b4f2f16615575de
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-03-11 09:06:27 +0530

    initial setup of hangman using vite + react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>
+        Made with love by <strong>Muhafiz Raza</strong> using Vite + React
+      </p>
+    </>
+  )
+}
+
+export default App
