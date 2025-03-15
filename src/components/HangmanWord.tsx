300e134f61e3b30c825a8631667c1b220d910519
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-03-15 09:07:34 +0530

    style word-to-guess

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,17 @@
+import "./HangmanWord.css";
+
 const HangmanWord = () => {
-    return <>
-        <p>This is Hangman Word.</p>
-    </>
+
+    const wordToGuess: string = "random";
+    return (
+        <div className="word-to-guess">
+            {wordToGuess.split("").map((char, index) => (
+                <div className="character-div"  key={index}>
+                    <span className="character-in-word-to-guess">{ char }</span>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default HangmanWord;
