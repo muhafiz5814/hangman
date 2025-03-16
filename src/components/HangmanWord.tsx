8378a4ea6b7752c69dec167cfa3342ccc9d1ba64
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-03-16 11:17:02 +0530

    rename and update alphabets array and show word in Uppercase.

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -4,7 +4,7 @@ const HangmanWord = ({wordToGuess }: {wordToGuess: string}) => {
 
     return (
         <div className="word-to-guess">
-            {wordToGuess.split("").map((char, index) => (
+            {wordToGuess.toUpperCase().split("").map((char, index) => (
                 <div className="character-div"  key={index}>
                     <span className="character-in-word-to-guess">{ char }</span>
                 </div>
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -2,12 +2,12 @@ import "./Keyboard.css";
 
 const Keyboard = ({setGuessedLetters}: {setGuessedLetters:React.Dispatch<React.SetStateAction<string[]>>}) => {
 
-    const alphabets: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const KEYS: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     return (
         <div className="keyboard-div">
-            {alphabets.map(alphabet => (
-                <button className="key" key={alphabet} onClick={() => setGuessedLetters((guessedLetters) => ([...guessedLetters, alphabet]))}>{alphabet}</button>
+            {KEYS.map(key => (
+                <button className="key" key={key} onClick={() => setGuessedLetters((guessedLetters) => ([...guessedLetters, key]))}>{key.toUpperCase()}</button>
             ))}
         </div>
     )
