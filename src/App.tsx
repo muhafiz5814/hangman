616415fa46944ee73311585195acec3607e985ac
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-03-23 13:33:46 +0530

    add keyboard event listeners of keypress.

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import { useEffect, useState } from 'react';
 
 import './App.css'
 
+const getWord = () => words[Math.floor(Math.random() * words.length)];
+
 function App() {
 
-  const [randomWord] = useState<string>(() => words[Math.floor(Math.random() * words.length)]);
+  const [randomWord, setRandomWord] = useState<string>(getWord);
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
@@ -17,16 +19,56 @@ function App() {
 
   const incorrectGuesses = guessedLetters.filter(letter => !randomWord.includes(letter));
 
-  useEffect(() => {
-    if (incorrectGuesses.length >= 6) setResult("lost");
-    if (randomWord.split("").every(char => guessedLetters.includes(char))) setResult("won");
-  }, [guessedLetters]);
-
   const addGuessedLetter = (letter: string) => {
     if (guessedLetters.includes(letter)) return;
 
     setGuessedLetters(currentLetters => [...currentLetters, letter]);
   }
+
+  useEffect(() => {
+    if (incorrectGuesses.length >= 6) setResult("lost");
+    if (randomWord.split("").every(char => guessedLetters.includes(char))) setResult("won");
+  }, [guessedLetters]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+
+      const key: string = e.key;
+
+      if (!key.match(/^[a-z]$/) || result) return;
+      
+      e.preventDefault();
+      addGuessedLetter(key);
+    }
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+  }, [guessedLetters, result]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+
+      const key: string = e.key;
+
+      if (key !== "Enter") return;
+
+      if (result === undefined) return;
+      
+      e.preventDefault();
+      setGuessedLetters([]);
+      setResult(undefined);
+      setRandomWord(getWord());
+    }
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+  }, [guessedLetters, result]);
 
   return (
     <div className='wrapper'>
